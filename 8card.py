877dcdd5c40812238cc25230f8a4e1a2bfc5bf95
natemeyer001 <30487641+natemeyer001@users.noreply.github.com>
2022-01-10 12:09:19 -0600

    removed analysis portion that was covered in 8cardAnalysis

--- 8card.py
+++ 8card.py
@@ -41,40 +41,10 @@ def run_game(deck,table):
 
 #MAIN
 result = []
-trials = 10000000 # One Million
+trials = 10000000 # Ten Million
 for i in range(trials):
     result.append(initiate())
 
 # export data as series to analyze
 result_series = pd.Series(result)
 result_series.to_csv("8_card_data.csv", index=False)
-
-"""
-ave_cards = round(float(sum(result))/len(result), 2)
-median_cards = statistics.median(result)
-print(f"The average is {ave_cards}  and the median is {median_cards}")
-# ave: 18.9  median: 16.0
-
-wins = sum(map(lambda x: x==52, result))
-win_perc = round((100*wins)/trials, 2)
-print(f"There were {wins} wins, which is {win_perc}%")
-# 540,594 wins which is 5.41%
-
-bad_loss = sum(map(lambda y: y==8, result))
-bad_loss_perc = round((100*bad_loss)/trials, 2)
-print(f"There were {bad_loss} losses without any pairs, which is {bad_loss_perc}%")
-
-highest_loss = -1
-for j in range(trials):
-	if((result[j] != 52) and (result[j] > highest_loss)):
-		highest_loss = result[j]
-print(f"The highest loss was at {highest_loss} cards")
-		
-# PLOT
-plt.hist(result, bins=45, density=True) # normalized
-plt.title("8-card garbage")
-plt.xlabel("Cards")
-plt.ylabel("Frequency") 
-plt.show()
-
-"""
